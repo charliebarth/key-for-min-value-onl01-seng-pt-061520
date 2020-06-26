4e42e7718676fa5ee3d14ab2f8705d14ea50e944
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 21:13:46 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
   new_array = []
   place_in_array = name_hash.size - 1
   name_hash.each do |key, value|
-    if value > current_value
+    if value < current_value
       new_array << key
       current_value = value
       else value < current_value
