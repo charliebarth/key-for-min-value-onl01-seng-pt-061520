76cdb99b176b8d1cbc1ca32fbbd792d12c1472f3
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 21:12:12 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,2 +1,12 @@
-hash = {"key" => 450, "vaule two" => 250}
-puts hash[450]
+def key_for_min_value(name_hash)
+  current_value = 0
+  new_array = []
+  place_in_array = name_hash.size - 1
+  name_hash.each do |key, value|
+    if value > current_value
+      new_array << key
+      current_value = value
+      else value < current_value
+    end
+  puts new_array[place_in_array]
+end
